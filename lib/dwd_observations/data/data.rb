f1bbf828d053be67952fb779e34d14183071e602
Benjamin Held <me@bheld.eu>
2020-11-19 20:28:10 +0100

    updated attribute documentation

--- lib/dwd_observations/data/data.rb
+++ lib/dwd_observations/data/data.rb
@@ -1,18 +1,18 @@
 # @Author: Benjamin Held
 # @Date:   2018-01-12 21:21:39
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2020-11-08 21:05:13
+# @Last Modified time: 2020-11-19 19:49:22
 
 module DwdObservations
 
   # Simple data class holding the common attributes of the observation data
   class Data
 
-    # the station id
+    # @return [Integer] the station id
     attr_accessor :station_id
-    # the given timestamp
+    # @return [Time] the given timestamp
     attr_accessor :timestamp
-    # the given quality level
+    # @return [Integer] the given quality level
     attr_accessor :quality_level
     
   end
