fe61fd3bee1ad2eab97f88b13b90ab1792552f38
Benjamin Held <me@bheld.eu>
2020-12-06 17:48:09 +0100

    added module requirements

--- lib/dwd_observations.rb
+++ lib/dwd_observations.rb
@@ -2,10 +2,15 @@
 # @Author: Benjamin Held
 # @Date:   2020-11-06 17:14:14
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2020-11-07 18:43:43
+# @Last Modified time: 2020-12-06 17:47:42
 
+# main module
 module DwdObservations
 
+  require "dwd_observations/data"
   require "dwd_observations/meta_data"
+  require "dwd_observations/parameter"
+  require "dwd_observations/reader"
+  require "dwd_observations/statistic"
 
 end
