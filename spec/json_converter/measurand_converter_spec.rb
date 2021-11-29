99693fe35e84517d228206ef12f59ec3225f67c2
Benjamin Held <me@bheld.eu>
2021-11-29 09:57:04 +0100

    add testcase for precipitation data

--- spec/json_converter/measurand_converter_spec.rb
+++ spec/json_converter/measurand_converter_spec.rb
@@ -8,7 +8,7 @@ describe DwdObservations::JsonConverter::MeasurandConverter do
     context "given temperature observation data of a station" do
       it "initialize converter and and create json output for the temperature" do
         reader = DwdObservations::TemperatureReader.new(
-                 File.join(__dir__,"../files/temp_hourly_00433.txt"), META_DATA.to_path)
+                 File.join(DATA_ROOT,"temp_hourly_00433.txt"), META_DATA.to_path)
         converter = DwdObservations::JsonConverter::MeasurandConverter.new(reader.data_repository)
         converter.convert(DATA_ROOT.to_path)
         expect(FileUtils.compare_file(File.join(DATA_ROOT,"output.json"), File.join(DATA_ROOT,"temperature_output.json"))).to be_truthy
@@ -23,7 +23,7 @@ describe DwdObservations::JsonConverter::MeasurandConverter do
     context "given pressure observation data of a station" do
       it "initialize converter and and create json output for the pressure" do
         reader = DwdObservations::PressureReader.new(
-                 File.join(__dir__,"../files/pressure_hourly_00433.txt"), META_DATA.to_path)
+                 File.join(DATA_ROOT,"pressure_hourly_00433.txt"), META_DATA.to_path)
         converter = DwdObservations::JsonConverter::MeasurandConverter.new(reader.data_repository)
         converter.convert(DATA_ROOT.to_path)
         expect(FileUtils.compare_file(File.join(DATA_ROOT,"output.json"), File.join(DATA_ROOT,"pressure_output.json"))).to be_truthy
@@ -34,5 +34,19 @@ describe DwdObservations::JsonConverter::MeasurandConverter do
     end
   end
 
+  describe ".convert" do
+    context "given precipitation observation data of a station" do
+      it "initialize converter and and create json output for the precipitation" do
+        reader = DwdObservations::PrecipitationReader.new(
+                 File.join(DATA_ROOT,"precip_hourly_00433.txt"), META_DATA.to_path)
+        converter = DwdObservations::JsonConverter::MeasurandConverter.new(reader.data_repository)
+        converter.convert(DATA_ROOT.to_path)
+        expect(FileUtils.compare_file(File.join(DATA_ROOT,"output.json"), File.join(DATA_ROOT,"precip_output.json"))).to be_truthy
+
+        # clean up data from the test and catch errors since they should not let the test fail
+        File.delete(File.join(DATA_ROOT,"output.json"))
+      end
+    end
+  end
 
 end
