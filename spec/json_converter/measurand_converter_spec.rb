41f720a456ec9cdb7b365fe039e25a0e0b782696
Benjamin Held <me@bheld.eu>
2021-11-29 09:24:14 +0100

    add testcase for pressure data

--- spec/json_converter/measurand_converter_spec.rb
+++ spec/json_converter/measurand_converter_spec.rb
@@ -5,7 +5,7 @@ require "dwd_observations/reader"
 describe DwdObservations::JsonConverter::MeasurandConverter do
 
   describe ".convert" do
-    context "given a temperature observation data of at station" do
+    context "given temperature observation data of a station" do
       it "initialize converter and and create json output for the temperature" do
         reader = DwdObservations::TemperatureReader.new(
                  File.join(__dir__,"../files/temp_hourly_00433.txt"), META_DATA.to_path)
@@ -18,5 +18,21 @@ describe DwdObservations::JsonConverter::MeasurandConverter do
       end
     end
   end
+
+  describe ".convert" do
+    context "given pressure observation data of a station" do
+      it "initialize converter and and create json output for the pressure" do
+        reader = DwdObservations::PressureReader.new(
+                 File.join(__dir__,"../files/pressure_hourly_00433.txt"), META_DATA.to_path)
+        converter = DwdObservations::JsonConverter::MeasurandConverter.new(reader.data_repository)
+        converter.convert(DATA_ROOT.to_path)
+        expect(FileUtils.compare_file(File.join(DATA_ROOT,"output.json"), File.join(DATA_ROOT,"pressure_output.json"))).to be_truthy
+
+        # clean up data from the test and catch errors since they should not let the test fail
+        File.delete(File.join(DATA_ROOT,"output.json"))
+      end
+    end
+  end
+
 
 end
