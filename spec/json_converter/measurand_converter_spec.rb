059f10585b8a3068c6d2e9603f51f6d683c705d0
Benjamin Held <me@bheld.eu>
2021-11-30 08:05:34 +0100

    add testcase for wind data

--- spec/json_converter/measurand_converter_spec.rb
+++ spec/json_converter/measurand_converter_spec.rb
@@ -49,4 +49,19 @@ describe DwdObservations::JsonConverter::MeasurandConverter do
     end
   end
 
+  describe ".convert" do
+    context "given wind observation data of a station" do
+      it "initialize converter and and create json output for the wind" do
+        reader = DwdObservations::WindReader.new(
+                 File.join(DATA_ROOT,"wind_hourly_00433.txt"), META_DATA.to_path)
+        converter = DwdObservations::JsonConverter::MeasurandConverter.new(reader.data_repository)
+        converter.convert(DATA_ROOT.to_path)
+        expect(FileUtils.compare_file(File.join(DATA_ROOT,"output.json"), File.join(DATA_ROOT,"wind_output.json"))).to be_truthy
+
+        # clean up data from the test and catch errors since they should not let the test fail
+        File.delete(File.join(DATA_ROOT,"output.json"))
+      end
+    end
+  end
+
 end
