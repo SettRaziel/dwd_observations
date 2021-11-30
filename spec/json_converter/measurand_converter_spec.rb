f5616c45f29f46d88c34bfaecf76e53129c58b05
Benjamin Held <me@bheld.eu>
2021-11-30 08:33:55 +0100

    add testcase for suntime data

--- spec/json_converter/measurand_converter_spec.rb
+++ spec/json_converter/measurand_converter_spec.rb
@@ -64,4 +64,19 @@ describe DwdObservations::JsonConverter::MeasurandConverter do
     end
   end
 
+  describe ".convert" do
+    context "given suntime observation data of a station" do
+      it "initialize converter and and create json output for the suntime" do
+        reader = DwdObservations::SuntimeReader.new(
+                 File.join(DATA_ROOT,"suntime_hourly_00433.txt"), META_DATA.to_path)
+        converter = DwdObservations::JsonConverter::MeasurandConverter.new(reader.data_repository)
+        converter.convert(DATA_ROOT.to_path)
+        expect(FileUtils.compare_file(File.join(DATA_ROOT,"output.json"), File.join(DATA_ROOT,"suntime_output.json"))).to be_truthy
+
+        # clean up data from the test and catch errors since they should not let the test fail
+        File.delete(File.join(DATA_ROOT,"output.json"))
+      end
+    end
+  end
+
 end
